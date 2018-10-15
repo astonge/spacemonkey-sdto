3bfa9160c31ea4caa7da7b171e766aa54f0e10bb
root <root@localhost.localdomain>
2018-10-14 20:26:44 -0400

    updates

--- amass.sh
+++ amass.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 touch $1-domains.txt
-amass -d $1 | tee $1-domains.txt
+~/go/bin/amass -norecursive -noalts -d $1 | tee $1-domains.txt
--- subjack.sh
+++ subjack.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 touch $1-scan
-subjack -v -a -w $1-domains.txt -ssl -a -o $1-scan
+conf_file=$HOME/go/src/github.com/haccer/subjack/fingerprints.json
+~/go/bin/subjack -c $conf_file -v -a -w $1-domains.txt -ssl -a -o $1-scan
+echo "Checking for Vulnerable subdomains.."
 cat $1-scan|grep -v "Not Vuln"
