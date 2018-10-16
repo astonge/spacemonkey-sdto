55c4f980c56ad5a54d4e65ac815d9a4a9fcba9b1
root <root@localhost.localdomain>
2018-10-15 21:48:33 -0400

    updates

--- amass.sh
+++ amass.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-touch $1-domains.txt
-~/go/bin/amass -norecursive -noalts -d $1 | tee $1-domains.txt
+touch $1-amass
+touch $1-subfinder
+~/go/bin/subfinder -o $1-subfinder -d $1
+~/go/bin/amass -norecursive -noalts -d $1 | tee $1-amass
+echo "subdomain enum done.."
+echo "splicing lists.."
+cat $1-amass >> $1-subfinder
+echo "sorting.."
+cat $1-subfinder|sort|uniq > $1-domains.txt
+echo "done."
