2ed61d553ea56efe540f133725d15fb520a1f4f3
astonge <astonge@gmail.com>
2018-11-12 15:19:50 -0500

    uncommenting things that i commented out. doh.

--- amass.sh
+++ amass.sh
@@ -3,8 +3,8 @@
 touch $1-amass
 touch $1-subfinder
 echo "[*] Finding Subdomains for $1"
-#~/go/bin/subfinder -silent -o $1-subfinder -d $1 1&>/dev/null
-#~/go/bin/amass -active -norecursive -noalts -d $1 -o $1-amass 1&>/dev/null
+~/go/bin/subfinder -silent -o $1-subfinder -d $1 1&>/dev/null
+~/go/bin/amass -active -norecursive -noalts -d $1 -o $1-amass 1&>/dev/null
 echo "[*] Subdomain enum Done"
 echo "[*] Splicing & Sorting Domains"
 cat $1-amass >> $1-subfinder
