577d540abb820227ac8b81167643e3b70439c79b
root <root@localhost.localdomain>
2018-11-12 15:17:14 -0500

    output updates to amass.sh

--- amass.sh
+++ amass.sh
@@ -2,11 +2,12 @@
 
 touch $1-amass
 touch $1-subfinder
-~/go/bin/subfinder -o $1-subfinder -d $1
-~/go/bin/amass -norecursive -noalts -d $1 | tee $1-amass
-echo "subdomain enum done.."
-echo "splicing lists.."
+echo "[*] Finding Subdomains for $1"
+#~/go/bin/subfinder -silent -o $1-subfinder -d $1 1&>/dev/null
+#~/go/bin/amass -active -norecursive -noalts -d $1 -o $1-amass 1&>/dev/null
+echo "[*] Subdomain enum Done"
+echo "[*] Splicing & Sorting Domains"
 cat $1-amass >> $1-subfinder
-echo "sorting.."
 cat $1-subfinder|sort|uniq > $1-domains.txt
-echo "done."
+num=$(cat $1-domains.txt|wc -l)
+echo "[*] $num domains. Done"
