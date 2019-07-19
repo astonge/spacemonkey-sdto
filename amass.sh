ad0cd3dbd8035f3f527f30624e8ca6665ff957e1
Adrian St. Onge <astonge@gmail.com>
2019-07-18 22:45:03 -0700

    updates? i dont fucking know..

--- amass.sh
+++ amass.sh
@@ -2,10 +2,13 @@
 
 touch $1-amass
 touch $1-subfinder
+
 echo "[*] Finding Subdomains for $1"
-~/go/bin/subfinder -silent -o $1-subfinder -d $1 1&>/dev/null
-~/go/bin/amass -active -norecursive -noalts -d $1 -o $1-amass 1&>/dev/null
+~/go/bin/subfinder -t 10 -silent -o $1-subfinder -d $1 1&>/dev/null
+#/snap/bin/amass -active -norecursive -noalts -d $1 -o $1-amass 1&>/dev/null
+/snap/bin/amass enum -active -d $1 -o $1-amass 1&>/dev/null
 echo "[*] Subdomain enum Done"
+
 echo "[*] Splicing & Sorting Domains"
 cat $1-amass >> $1-subfinder
 cat $1-subfinder|sort|uniq > $1-domains.txt
--- subjack.sh
+++ subjack.sh
@@ -3,4 +3,4 @@ touch $1-scan
 conf_file=$HOME/go/src/github.com/haccer/subjack/fingerprints.json
 ~/go/bin/subjack -c $conf_file -v -a -w $1-domains.txt -ssl -a -o $1-scan
 echo "Checking for Vulnerable subdomains.."
-cat $1-scan|grep -v "Not Vuln"
+cat $1-scan|grep -v "Not Vuln"|tee $1-vuln
