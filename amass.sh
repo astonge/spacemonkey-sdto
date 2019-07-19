73dd952acb808e513cc014956f176a1ce7f3805e
Adrian St. Onge <astonge@gmail.com>
2019-07-19 00:00:57 -0700

    udpates

--- amass.sh
+++ amass.sh
@@ -4,13 +4,18 @@ touch $1-amass
 touch $1-subfinder
 
 echo "[*] Finding Subdomains for $1"
+echo "... subfinder"
 ~/go/bin/subfinder -t 10 -silent -o $1-subfinder -d $1 1&>/dev/null
-#/snap/bin/amass -active -norecursive -noalts -d $1 -o $1-amass 1&>/dev/null
+echo "... amass (active)"
 /snap/bin/amass enum -active -d $1 -o $1-amass 1&>/dev/null
+echo "... amass (brute)"
+/snap/bin/amass enum -src -brute -min-for-recursive 1 -o $1-amass-brute -d $1 1&>/dev/null
 echo "[*] Subdomain enum Done"
 
 echo "[*] Splicing & Sorting Domains"
 cat $1-amass >> $1-subfinder
-cat $1-subfinder|sort|uniq > $1-domains.txt
+cat $1-amass-brute >> $1-subfinder
+
+cat $1-subfinder|sort|uniq | sed -E 's/\[.*\]//g' |awk '{$1=$1};1' > $1-domains.txt
 num=$(cat $1-domains.txt|wc -l)
 echo "[*] $num domains. Done"
--- subjack.sh
+++ subjack.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 touch $1-scan
 conf_file=$HOME/go/src/github.com/haccer/subjack/fingerprints.json
-~/go/bin/subjack -c $conf_file -v -a -w $1-domains.txt -ssl -a -o $1-scan
+~/go/bin/subjack -c $conf_file -v -t 15 -m -w $1-domains.txt -ssl -a -o $1-scan
 echo "Checking for Vulnerable subdomains.."
 cat $1-scan|grep -v "Not Vuln"|tee $1-vuln
